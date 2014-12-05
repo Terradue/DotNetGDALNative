b52c5cc65d3dd88686bfcdc374472cdc65609daf
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2014-12-05 15:18:40 +0100

    p
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- patches/patch.sh
+++ patches/patch.sh
@@ -7,8 +7,8 @@ patch -i $PATCH_DIR/Band.cs.patch $GDAL_CSHARP_DIR/gdal/Band.cs
 patch -i $PATCH_DIR/Dataset.cs.patch $GDAL_CSHARP_DIR/gdal/Dataset.cs
 patch -i $PATCH_DIR/Driver.cs.patch $GDAL_CSHARP_DIR/gdal/Driver.cs
 
-patch -i $PATCH_DIR/OgrPINVOKE.cs.patch $GDAL_CSHARP_DIR/ogr/OgrPINVOKE.cs
-patch -i $PATCH_DIR/OsrPINVOKE.cs.patch $GDAL_CSHARP_DIR/osr/OsrPINVOKE.cs
-patch -i $PATCH_DIR/GdalPINVOKE.cs.patch $GDAL_CSHARP_DIR/gdal/GdalPINVOKE.cs
+#patch -i $PATCH_DIR/OgrPINVOKE.cs.patch $GDAL_CSHARP_DIR/ogr/OgrPINVOKE.cs
+#patch -i $PATCH_DIR/OsrPINVOKE.cs.patch $GDAL_CSHARP_DIR/osr/OsrPINVOKE.cs
+#patch -i $PATCH_DIR/GdalPINVOKE.cs.patch $GDAL_CSHARP_DIR/gdal/GdalPINVOKE.cs
 
 exit 0
