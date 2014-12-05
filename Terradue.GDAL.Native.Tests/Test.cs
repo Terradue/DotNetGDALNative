527f60b334349703fc377825fc86d2f4e8b36e5a
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2014-12-05 14:43:32 +0100

    Tests
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

---
+++ Terradue.GDAL.Native.Tests/Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Terradue.GDAL.Native.Tests {
+    [TestFixture()]
+    public class Test {
+
+        [TestFixtureSetUp]
+        public void GdalConfig() {
+
+            try {
+                GdalConfiguration.ConfigureOgr();
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        [Test()]
+        public void TestCase() {
+        }
+    }
+}
+
--- Terradue.GDAL.Native/GdalConfiguration.cs
+++ Terradue.GDAL.Native/GdalConfiguration.cs
@@ -60,31 +60,13 @@ namespace Terradue.GDAL
             if (string.IsNullOrEmpty(executingDirectory))
                 throw new InvalidOperationException("cannot get executing directory");
 
-
             var gdalPath = Path.Combine(executingDirectory, "gdal");
-            var nativePath = Path.Combine(gdalPath, GetPlatform());
 
             // Prepend native path to environment path, to ensure the
             // right libs are being used.
-            var path = Environment.GetEnvironmentVariable("PATH");
-            path = nativePath + ";" + Path.Combine(nativePath, "plugins") + ";" + path;
-            Environment.SetEnvironmentVariable("PATH", path);
-
-            // Set the additional GDAL environment variables.
-            var gdalData = Path.Combine(gdalPath, "data");
-            Environment.SetEnvironmentVariable("GDAL_DATA", gdalData);
-            Gdal.SetConfigOption("GDAL_DATA", gdalData);
-
-            var driverPath = Path.Combine(nativePath, "plugins");
-            Environment.SetEnvironmentVariable("GDAL_DRIVER_PATH", driverPath);
-            Gdal.SetConfigOption("GDAL_DRIVER_PATH", driverPath);
-
-            Environment.SetEnvironmentVariable("GEOTIFF_CSV", gdalData);
-            Gdal.SetConfigOption("GEOTIFF_CSV", gdalData);
-
-            var projSharePath = Path.Combine(gdalPath, "share");
-            Environment.SetEnvironmentVariable("PROJ_LIB", projSharePath);
-            Gdal.SetConfigOption("PROJ_LIB", projSharePath);
+            var path = Environment.GetEnvironmentVariable("LD_LIBRARY_PATH");
+            path = executingDirectory + ":" + Path.Combine(gdalPath, "plugins") + ":" + path;
+            Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", path);
         }
 
         /// <summary>
